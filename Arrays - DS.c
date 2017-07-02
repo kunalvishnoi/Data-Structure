86bdea01470ea77ba3b091d4a52218ff8f22c99b
kunalvishnoi <vishnoi.kunal0@gmail.com>
2017-07-02 23:18:01 +0530

    Hackerrank DS Solution

---
+++ Arrays - DS.c
@@ -0,0 +1,22 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main(){
+    int n,i,arr[10000];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=n-1;i>=0;i--)
+    {
+        printf("%d ",arr[i]);
+    }
+    
+    return 0;
+}
